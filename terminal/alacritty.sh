25ba108bf79804c0bd677f3e34a0d71e4ce1baaa
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2025-06-05 19:56:54 +0100

    feat: added alacritty config

---
+++ terminal/alacritty.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install alacritty and nerd fonts
+sudo pacman -S alacritty nerd-fonts --noconfirm
+
+# Backup existent alacritty config
+mv $HOME/.config/alacritty $HOME/.config/alacritty.bak
+
+# Create a symlink for alacritty config
+ln -s $(realpath $0)/../configs/terminal/alacritty $HOME/.config/alacritty
