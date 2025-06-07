8fd4c04b565e2b08db3f9cd56d698d706a4f4d32
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2025-06-07 22:51:38 +0100

    fix: added missing package and fixed symlink

--- wm/theming.sh
+++ wm/theming.sh
@@ -5,7 +5,7 @@ link_path() {
 }
 
 # Install required packages
-sudo pacman -S gtk-engine-murrine --noconfirm
+sudo pacman -S gtk-engine-murrine wget --noconfirm
 
 # Backup previous GTK configs
 mv $HOME/.gtkrc-2.0 $HOME/.gtkrc-2.0.bak
@@ -14,7 +14,7 @@ mv $HOME/.config/gtk-3.0 $HOME/.config/gtk-3.0.bak
 # Download and install GTK theme
 tmp_dir=$(mktemp -d)
 git clone https://github.com/Fausto-Korpsvart/Tokyonight-GTK-Theme.git $tmp_dir/Tokyonight-GTK-Theme
-$tmp_dir/Tokyonight-GTK-Theme/install.sh -l
+$tmp_dir/Tokyonight-GTK-Theme/themes/install.sh -l
 
 # Download and install Icon theme
 wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$HOME/.icons" sh
@@ -24,5 +24,5 @@ git clone https://github.com/vinceliuice/McMojave-cursors.git $tmp_dir/McMojave-
 $tmp_dir/McMojave-cursors/install.sh
 
 # Create a symlink for GTK config files
-ln -s $(link_path "configs/wm/theming/.gtkrc-2.0") $HOME
-ln -s $(link_path "configs/wm/theming/gtk-3.0") $HOME/.config
+ln -s $(link_path "../configs/wm/theming/.gtkrc-2.0") $HOME
+ln -s $(link_path "../configs/wm/theming/gtk-3.0") $HOME/.config
