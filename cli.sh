b1846003debfd2cb8cee30a436077751d06e0e72
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2025-07-06 17:25:45 +0100

    feat: added cli apps

---
+++ cli.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo pacman -S htop           \
+                libqalculate  \
+                ncdu          \
+                eza           \
+                bat           \
+                ripgrep       \
+                fd            \
+                httpie        \
+                jq            \
+                zip           \
+                unzip         \
+                fzf           \
+                tealdeer      \
+                tree          \
+                wget          \
+                curl          \
