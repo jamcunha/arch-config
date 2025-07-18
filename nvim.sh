4713e0be64770637cf2a3dc2b797400c8821871e
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2025-07-18 21:59:56 +0100

    feat: added nvim

---
+++ nvim.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+link_path() {
+  echo $(readlink -f "$(dirname "$(realpath $0)")/$1" 2>/dev/null || echo "$(dirname "$(realpath $0)")/$1")
+}
+
+# Install dependencies
+sudo pacman -S base-devel cmake ninja curl
+
+tmp_dir=$(mktemp -d)
+git clone https://github.com/neovim/neovim.git $tmp_dir/neovim
+cd $tmp_dir/neovim
+
+# Install nvim
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+
+cd -
+
+# Create a symlink for nvim config
+mv $HOME/.config/nvim $HOME/.config/nvim.bak
+ln -s $(link_path "configs/nvim") $HOME/.config
