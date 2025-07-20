7339e824117b6117c3537fcbdcc2b55e448a1605
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2025-07-20 18:12:12 +0100

    feat: added protopup-qt to gaming script

--- gaming.sh
+++ gaming.sh
@@ -3,6 +3,9 @@
 # Install cachyos meta packages
 sudo pacman -S cachyos-gaming-meta cachyos-gaming-applications --noconfirm
 
+# Install protonup-qt
+sudo pacman -S protonup-qt --noconfirm
+
 # Install Microsoft fonts
 paru -S ttf-ms-win11-auto --noconfirm
 
