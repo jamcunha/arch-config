7a74e58831958988da14683eac0b3ef0c5945ba5
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2025-08-09 19:26:36 +0100

    feat: added programming packages

--- configs/zsh/.zshrc
+++ configs/zsh/.zshrc
@@ -62,3 +62,7 @@ eval "$(fzf --zsh)"
 
 # Add paths
 export PATH="$PATH:$HOME/.local/share/bin"
+
+# Load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
---
+++ programming.sh
@@ -0,0 +1,33 @@
+# C unit testing
+paru -S criterion --noconfirm
+
+# C#
+sudo pacman -S dotnet-sdk --noconfirm
+
+# Go
+sudo pacman -S go         \
+                go-tools  \
+                --noconfirm
+
+# Air
+go install github.com/air-verse/air@latest
+
+# Sqlc
+sudo pacman -S sqlc --noconfirm
+
+# Goose
+paru -S goose --noconfirm
+
+# Lua
+sudo pacman -S lua --noconfirm
+
+# Python
+sudo pacman -S python       \
+                python-pip  \
+                --noconfirm
+
+# Node.js
+sudo pacman -S nvm --noconfirm
+
+# Rust
+sudo pacman -S rustup --noconfirm
